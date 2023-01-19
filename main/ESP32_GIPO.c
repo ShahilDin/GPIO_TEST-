c764a804891b4dece2cc5058f3c7435591614d95
Shahil Din <dinshahil@gmail.com>
2023-01-20 08:28:40 +1300

    Added Comments

--- main/ESP32_GIPO.c
+++ main/ESP32_GIPO.c
@@ -1,21 +1,23 @@
-#include <stdio.h>
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <stdio.h> // Standar Input/Output Header File 
+#include "driver/gpio.h" // This file contains declarations for functions and macros that are specific to the gpio of the ESP32 device. allows to control the GPIO
+#include "freertos/FreeRTOS.h" //FreeRTOS is a real-time operating system kernel for microcontrollers, it allows to create task, semaphores, queue and other real-time related functionality. 
+#include "freertos/task.h"///Delay function 
 
 void app_main(void)
 {
+    // setting the input/output for the GPIO pin
     gpio_set_direction(GPIO_NUM_2,GPIO_MODE_OUTPUT);
     gpio_set_direction(GPIO_NUM_0,GPIO_MODE_OUTPUT);
     gpio_set_direction(GPIO_NUM_4,GPIO_MODE_OUTPUT);
     
     while(true)
     {
+        // setting the value of those output to run the RGB 
         gpio_set_level(GPIO_NUM_0,1);
         gpio_set_level(GPIO_NUM_2,0);
         gpio_set_level(GPIO_NUM_4,0);
 
-        vTaskDelay(100);
+        vTaskDelay(100); //delay 
 
         gpio_set_level(GPIO_NUM_0,0);
         gpio_set_level(GPIO_NUM_2,1);
